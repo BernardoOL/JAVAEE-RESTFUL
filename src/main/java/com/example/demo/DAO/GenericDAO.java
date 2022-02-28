866cdf18a8774411f0ae3c20b808420cc983a571
Jhonnatan Mesquita <jhonnatanmesquita@gmail.com>
2022-02-28 17:28:20 -0300

    first commit

---
+++ src/main/java/com/example/demo/DAO/GenericDAO.java
@@ -0,0 +1,53 @@
+package com.example.demo.DAO;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.transaction.Transactional;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Transactional
+public class GenericDAO<T, I extends Serializable>  {
+
+
+    @PersistenceContext(unitName = "PU")
+    EntityManager em;
+
+    private Class<T> persistedClass;
+
+    public GenericDAO(){
+    }
+
+    public GenericDAO(Class<T> persistedClass){
+        this();
+        this.persistedClass = persistedClass;
+    }
+
+    public void salvar (T t){
+        em.persist(t);
+    }
+
+    public T buscarId(I id){
+        return em.find(persistedClass, id);
+    }
+
+    public List<T> buscarTodos(){
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<T> query = builder.createQuery(persistedClass);
+        query.from(persistedClass);
+        return em.createQuery(query).getResultList();
+    }
+
+    public T atualizar(T t){
+        return em.merge(t);
+    }
+
+    public void deletar (I id){
+        T t = buscarId(id);
+        em.remove(t);
+    }
+}
+
